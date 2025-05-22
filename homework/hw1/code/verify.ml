ee2a2fd656533527fc7025eab09da7070522624c
sunbeomso <sunbeomso@gist.ac.kr>
2025-05-22 15:48:40 +0900

    fix error in the hw1 template

--- homework/hw1/code/verify.ml
+++ homework/hw1/code/verify.ml
@@ -14,10 +14,10 @@ type res = int * Z3.Model.model
 let run : pgm -> res option
 = fun pgm ->
   let rec loop (cur_bound:int) : res option =
-    if cur_bound = !Options.max_bound then None
+    if cur_bound > !Options.max_bound then None
     else begin
       Printf.eprintf "[INFO] current bound: %d\n%!" cur_bound;
-      assert (0 < cur_bound && cur_bound < !Options.max_bound);
+      assert (0 < cur_bound && cur_bound <= !Options.max_bound);
 
       let vc = gen_vc pgm cur_bound in
       match Solver.check_sat vc with
