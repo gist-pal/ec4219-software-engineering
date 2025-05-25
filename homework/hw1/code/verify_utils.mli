9cfe4723f7ff8dedf5fa7d903d9cbf8ae6a312f1
sunbeomso <sunbeomso@gist.ac.kr>
2025-05-25 10:54:20 +0900

    hw1: fix typo in the template

--- homework/hw1/code/verify_utils.mli
+++ homework/hw1/code/verify_utils.mli
@@ -7,5 +7,5 @@ val v3 : Pgm.tid * int * int -> Formula.formula
 (* create a variable A_lt for representing a thread A is in state l at time t *)
 val a : int * int -> Formula.formula
 
-(* create a variable B_lt for representing a thread A is in state l at time t *)
+(* create a variable B_lt for representing a thread B is in state l at time t *)
 val b : int * int -> Formula.formula
